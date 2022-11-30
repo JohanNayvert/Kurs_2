7fcc92fe6ed4597420d838ff8b8dc53b5f5ff412
JohanNayvert <111690307+JohanNayvert@users.noreply.github.com>
2022-11-30 17:34:45 +0500

    HomeWork Complete

---
+++ src/com/skypro/OOP_Polimorfizm/Drivers/Drivers.java
@@ -0,0 +1,64 @@
+package com.skypro.OOP_Polimorfizm.Drivers;
+
+import static com.skypro.OOP_Incapsul.task_1.ValidateUtils.validateDouble;
+import static com.skypro.OOP_Incapsul.task_1.ValidateUtils.validateString;
+
+public abstract class Drivers {
+
+    private final String fullName;
+    private final String driverLicense;
+    private double experience;
+
+    public Drivers(String fullName, String driverLicense, double experience) {
+        this.fullName = validateName(fullName);
+        this.driverLicense = validateDriverLicense(driverLicense);
+        this.experience = validateExperience(experience);
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    private String validateName(String value) {
+        return validateString(value, "Не указана");
+    }
+
+    public String getDriverLicense() {
+        return driverLicense;
+    }
+
+    private String validateDriverLicense(String value) {
+        return validateString(value, "Не указана");
+    }
+
+    public double getExperience() {
+        return experience;
+    }
+
+    public void setExperience(double experience) {
+        this.experience = validateExperience(experience);
+    }
+
+    private double validateExperience(double value) {
+        return validateDouble(value, 0);
+    }
+
+    void startMovingInDriver() {
+        System.out.println("Водитель начал движние");
+    }
+
+    void finishMovingInDriver() {
+        System.out.println("Водитель закончил движние");
+    }
+
+    void refuelCar() {
+        System.out.println("Водитель заправился");
+    }
+
+    @Override
+    public String toString() {
+        return "Полное имя: " + fullName +
+                ", категория прав: " + driverLicense +
+                ", опыт вождения: " + experience;
+    }
+}
---
+++ src/com/skypro/OOP_Polimorfizm/Transport/Buses.java
@@ -0,0 +1,30 @@
+package com.skypro.OOP_Polimorfizm.Transport;
+
+public class Buses extends Transport implements Competing {
+    public Buses(String brand, String model, double engineVolume) {
+        super(brand, model, engineVolume);
+    }
+
+    public String startMoving() {
+        return "Автобус начал движние";
+    }
+
+    public String finishMoving() {
+        return "Автобус закончил движние";
+    }
+
+    @Override
+    public void pitStop() {
+        System.out.println("Заезжает на питстоп");
+    }
+
+    @Override
+    public void bestTimelap() {
+        System.out.println("Лучший круг");
+    }
+
+    @Override
+    public void maxSpeed() {
+        System.out.println("Лучшая скорость");
+    }
+}
